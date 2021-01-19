f0350f4e2a2dbec9f572cc23fe6030551e1387d1
jamesjr19 <ruwartja@gmail.com>
2021-01-18 19:16:31 -0500

    RollDie() working and displaying to console.

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -7,20 +7,32 @@ namespace BattleArena
     {
         static void Main(string[] args)
         {
-        int stamina = 10;
-        int panicLevel = 10;
-        int enemyHealth = 10;
+            int stamina = 10;
+            int panicLevel = 10;
+            int enemyHealth = 10;
             Console.WriteLine("Welcome to the Battle Arean!");
             Console.WriteLine("Here are your starting stats:");
             DisplayHealthStats(stamina, panicLevel, enemyHealth);
-            string play = GetUserInput("Are you ready to roll the dice? (y/n) ").ToLower();
-            if (play == "y")
+
+            int rollCount = 0;
+            while (rollCount <= 10)
             {
-                int rollCount = 0;
-                while (rollCount <= 10)
+                string play = GetUserInput("Are you ready to roll? (y/n) ").ToLower();
+                if (play == "y")
                 {
-                    RollDie();
+
+                    int rollResult = RollDie();
+                    Console.WriteLine($"You rolled a {rollResult}.");
+
                     rollCount++;
+                    int rollsLeft = 10 - rollCount;
+                    RollResult(rollResult, stamina, panicLevel, enemyHealth);
+                    BattleResults(rollCount, stamina, panicLevel, enemyHealth);
+
+                    Console.WriteLine($"You have {rollsLeft} rolls left.\n");
+                    Console.WriteLine("Current Stats:");
+                    DisplayHealthStats(stamina, panicLevel, enemyHealth);
+
                 }
             }
         }
@@ -35,39 +47,39 @@ namespace BattleArena
         {
             //The user has 10 rolls of the die to defeat the enemy. 
             Random diceRoll = new Random();
-            return diceRoll.Next(1, 7); 
+            return diceRoll.Next(1, 7);
         }
 
-        public void RollResult(int diceRoll, int stamina, int panicLevel, int enemyHealth)
+        public static void RollResult(int rollResult, int stamina, int panicLevel, int enemyHealth)
         {
-            if (diceRoll == 1)
+            if (rollResult == 1)
             {
-                Console.WriteLine("The user gets hit, and their panic level increases by 2");
+                Console.WriteLine("The user gets hit, and their panic level increases by 2.\n");
                 panicLevel = panicLevel + 2;
             }
-            else if (diceRoll == 2)
+            else if (rollResult == 2)
             {
-                Console.WriteLine("The user dodges the attack, but their stamina is reduced by 1");
+                Console.WriteLine("The user dodges the attack, but their stamina is reduced by 1.\n");
                 stamina--;
             }
-            else if (diceRoll == 3)
+            else if (rollResult == 3)
             {
-                Console.WriteLine("The user deflects the attack. There is no stat change");
+                Console.WriteLine("The user deflects the attack. There is no stat change.\n");
             }
-            else if (diceRoll == 4)
+            else if (rollResult == 4)
             {
-                Console.WriteLine("The enemy backs away; the user’s stamina increases by 1");
+                Console.WriteLine("The enemy backs away; the user’s stamina increases by 1.\n");
                 stamina++;
             }
-            else if (diceRoll == 5)
+            else if (rollResult == 5)
             {
-                Console.WriteLine("The enemy dodges, but the user strikes a glancing blow. The user’s panic is reduced by 1. The enemy loses 3 hit points.");
+                Console.WriteLine("The enemy dodges, but the user strikes a glancing blow. \nThe user’s panic is reduced by 1. The enemy loses 3 hit points.\n");
                 panicLevel--;
                 enemyHealth = enemyHealth - 3;
             }
-            else if (diceRoll == 6)
+            else if (rollResult == 6)
             {
-                Console.WriteLine("The enemy takes significant damage. The user’s panic is reduced by 3. Their stamina is increased by 2. The enemy loses 5 hit points.");
+                Console.WriteLine("The enemy takes significant damage. The user’s panic is reduced by 3. \nTheir stamina is increased by 2. The enemy loses 5 hit points.\n");
                 panicLevel = panicLevel - 3;
                 stamina = stamina + 2;
                 enemyHealth = enemyHealth - 5;
@@ -79,7 +91,7 @@ namespace BattleArena
         {
             Console.WriteLine($"STAMINA: {stamina}");
             Console.WriteLine($"PANIC LEVEL: {panicLevel}");
-            Console.WriteLine($"ENEMY HEALTH: {enemyHealth}");
+            Console.WriteLine($"ENEMY HEALTH: {enemyHealth}\n");
         }
 
 
@@ -88,7 +100,7 @@ namespace BattleArena
         //If the enemy has died, another enemy enters the arena.The player’s panic and stamina stay where it is. 
         //If the player has run out of turns, their panic reaches the max level.
         //If the player has died, the dream starts over from the beginning.
-        public void BattleResults(int rollCount, int stamina, int panicLevel, int enemyHealth)
+        public static void BattleResults(int rollCount, int stamina, int panicLevel, int enemyHealth)
         {
             if (enemyHealth != 0)
             {
@@ -116,6 +128,6 @@ namespace BattleArena
 
         //}
     }
-                //Thread.Sleep(1000);
+    //Thread.Sleep(1000);
 }
 
